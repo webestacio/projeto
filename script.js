cc5baa2c705880987c3e0fe9c5465fc580b8086e
João Vitor Maranhão <joaovitordsmaranhao@gmail.com>
2025-09-04 22:33:58 -0300

    pagina de esqueci a senha front end e algumas coisas do backend, como verificador de conteudo das caixas inputs e criei funçao para a foto ao clicar voltar pro login

--- script.js
+++ script.js
@@ -9,7 +9,27 @@ function mostrar() {
     }
 function submit() {
    const email = document.getElementById('email').value
-   const senha = document.getElementById('senha').value
+      if (email == '') {
+      alert("você precisa digitar um email")
+   }
 
-   alert('Email: ' + email + "\nsenha: " + senha)
+   const senha = document.getElementById('senha').value
+      if (senha == '') {
+      alert("você precisa digitar uma senha")
+   }
+   else
+      alert('Email: ' + email + "\nsenha: " + senha)
+}
+
+function recuperar() {
+   const email = document.getElementById('email_recuperacao').value
+   if (email == '') {
+      alert("você precisa digitar um email")
+   }
+   else 
+       alert('email enviado para: ' + email)
+}
+
+function inicio() {
+   window.location.href = "login.html";
 }

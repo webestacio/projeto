bcf61d8430b922012c70f4e0b48631df31eeaf7d
João Vitor Maranhão <joaovitordsmaranhao@gmail.com>
2025-09-09 14:31:04 -0300

    melhorei o visual do codigo, adicionei tags de html novas, atualizei o css para ficar responsivo e mais bonito,retirei coisas futeis de java script e deixei o codigo mais limpo

--- script.js
+++ script.js
@@ -7,28 +7,6 @@ function mostrar() {
         campo.type  ="password";
      }
     }
-function submit() {
-   const email = document.getElementById('email').value
-      if (email == '') {
-      alert("você precisa digitar um email")
-   }
-
-   const senha = document.getElementById('senha').value
-      if (senha == '') {
-      alert("você precisa digitar uma senha")
-   }
-   else
-      alert('Email: ' + email + "\nsenha: " + senha)
-}
-
-function recuperar() {
-   const email = document.getElementById('email_recuperacao').value
-   if (email == '') {
-      alert("você precisa digitar um email")
-   }
-   else 
-       alert('email enviado para: ' + email)
-}
 
 function inicio() {
    window.location.href = "login.html";
